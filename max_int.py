1867c24cf019eeaea26b2f1dd13cd4483f0fd2b1
onyth23 <onyth23@gmail.com>
2018-09-04 14:19:23 +0000

    fixed a bug causing newline

--- max_int.py
+++ max_int.py
@@ -1,12 +1,14 @@
 # Fill in the missing code
 # Changes to test commit on github
 
+num_int = int(input("Input a number: "))    # Do not change this line
+
 max_int = 0
 num_int = 0
 
 while num_int >= 0:
-    num_int = int(input("Input a number: "))    # Do not change this line
     if num_int > max_int:
         max_int = num_int
+    num_int = int(input("Input a number: "))    
 
 print("The maximum is", max_int)    # Do not change this line
